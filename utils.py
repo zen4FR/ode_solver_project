8f40012f68ed79c44c9b1ce99a241418f73a0cf9
shreyashayushmhto <mahatoshreyash@gmail.com>
2025-06-19 17:58:49 +0545

    remaking it for supporting all

--- utils.py
+++ utils.py
@@ -1,28 +1,32 @@
+import matplotlib
+matplotlib.use('Agg')  # Use non-GUI backend (important for macOS)
+
 import matplotlib.pyplot as plt
 import io
 import base64
 import csv
 
 def create_plot(x_vals, y_vals, method):
-    plt.figure()
-    plt.plot(x_vals, y_vals, marker='o')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title(f'Solution using {method} Method')
-    plt.grid(True)
+    # Set up a headless plot
+    fig, ax = plt.subplots()
+    ax.plot(x_vals, y_vals, marker='o')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_title(f'Solution using {method} Method')
+    ax.grid(True)
 
-    # Save plot as PNG image
+    # Save plot to memory (for base64 string)
     img = io.BytesIO()
-    plt.savefig(img, format='png')
+    fig.savefig(img, format='png')
     img.seek(0)
     plot_url = base64.b64encode(img.getvalue()).decode()
-    plt.close()
-    
-    # Save in exports/static/output.png too
+    plt.close(fig)
+
+    # Save the plot as a static file (optional)
     with open('static/output.png', 'wb') as f:
         f.write(img.getvalue())
 
-    # Save the numerical results to exports/results.csv
+    # Save results to CSV
     with open('exports/results.csv', 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(['x', 'y'])
